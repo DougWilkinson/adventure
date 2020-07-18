2c8ae62539cd65e9dc65bb612213843446c80d9e
Doug Wilkinson <Doug_Wilkinson@brown.edu>
2020-07-18 14:24:56 -0400

    Added some comments

--- test-file.py
+++ test-file.py
@@ -1,2 +1,6 @@
 # This is a test file for github training
 # Add something below
+
+# This line is part of the branch called "dougs-test"
+# I can push this as a new branch to be reviewed before 
+# commit to master branch
